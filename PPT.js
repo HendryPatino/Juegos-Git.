68dace4ac22c19107093f04be08303356dc6fef9
Hendry Javier Patiño Muñoz <hendryjavier8@gmail.com>
2026-03-02 12:05:12 -0500

    Refactor player variables and game logic

--- PPT.js
+++ PPT.js
@@ -1,11 +1,16 @@
-let player1 = "tijeras"
-let player2 = "papel" 
+let Persona1 = "tijeras"
+let persona2 = "piedra"
 
 
-if (Player1 = "tijeras" &&& player2 = "papel") consolé.log("Jugador 1 gana.") : 
+if (persona1 === "piedra" && persona2 === "tijeras") {console.log("Gana jugador 1")} else
 
-if (Player2 = "tijeras" &&& player1 = "papel") consolé.log("Jugador 2 gana."): 
+    if (persona1 === "tijeras" && persona2 === "papel") {console.log("Gana jugador 1")} else 
 
-if (player1 === player2 ) Console.log ("empate") :
+        if (persona1 === "papel" && persona2 === "piedra") {console.log("Gana jugador 1")} else {
+}
+if (persona1 === "papel" && persona2 === "tijeras") {console.log("Gana jugador 2")} else
 
-if 
+    if (persona1 === "tijeras" && persona2 === "piedra") {console.log("Gana jugador 2")} else
+
+        if (persona1 === "piedra" && persona2 === "papel") {console.log("Gana jugador 2")} else
+            if (persona1 === persona2) {console.log("Nadie gana") ;}
