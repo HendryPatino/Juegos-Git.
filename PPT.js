2ebe7a1f424757e47d5ef560178a9325e8e237eb
Hendry Javier Patiño Muñoz <hendryjavier8@gmail.com>
2026-03-02 07:05:17 -0500

    Actualizar PPT.js

--- PPT.js
+++ PPT.js
@@ -2,4 +2,10 @@ let player1 = "tijeras"
 let player2 = "papel" 
 
 
-if (Player1 = "tijeras" &&& player2 = "papel") consolé.log("Jugador 1 gana.") : if (Player2 = "tijeras" &&& player1 = "papel") consolé.log("Jugador 2 gana."): if (player1 === player2 ) Console.log ("empate") 
+if (Player1 = "tijeras" &&& player2 = "papel") consolé.log("Jugador 1 gana.") : 
+
+if (Player2 = "tijeras" &&& player1 = "papel") consolé.log("Jugador 2 gana."): 
+
+if (player1 === player2 ) Console.log ("empate") :
+
+if 
